d3f9f3bdcc53ff03b12bb997ac8f3eb7f86a9ccd
koussayinsat12 <kousai.ghaouari@insat.ucar.tn>
2023-09-22 00:19:56 +0200

    controller change

---
+++ src/tdod/entities/tdod.entity.ts
@@ -0,0 +1,5 @@
+export class Tdod{
+id:number;
+name:string;
+description:string;
+}
--- src/tdod/tdod.controller.ts
+++ src/tdod/tdod.controller.ts
@@ -1,28 +1,50 @@
-import { Controller,Delete,Get, Post, Req, Res } from '@nestjs/common';
+import { Body, Controller,Delete,Get, Param, Post, Query, Req, Res } from '@nestjs/common';
 import { response } from 'express';
+import { Tdod } from './entities/tdod.entity';
+import { NotFoundError } from 'rxjs';
 
 @Controller('tdod')
 export class TdodController {
-
-
+    constructor(){
+        this.tdodos=[];
+    }
+    tdodos:Tdod[];
     @Get()
     getTodos(
-
-    ){
-        console.log("Recuperer tous les todos");
-        return "La liste des todos"
+        @Query() mesquery
+    ){  
+        console.log(mesquery)
+        return this.tdodos;
     }
+    @Get('/:id')
+    getTodosbyid(
+        @Param('id') id
+    ){
+        const todo = this.tdodos.find((actualtodo) => actualtodo.id === +id);
+        if (todo) {
+            return todo;
+        }
+        throw new NotFoundError(`Le todo d'ID ${id} n'existe pas`);
+    }
+    
     @Delete()
     deleteTodos(){
         console.log("supprimer tous les todos");
-        return "Liste de todos";
+        return "todos deleted";
     }
     @Post()
-    addTodos(){
-        console.log("ajouter tous les todos");
-        return "Liste de todos";
+    addTodos(
+        @Body() newTdodo: Tdod
+
+    ){
+        if (this.tdodos.length){
+            newTdodo.id=this.tdodos[this.tdodos.length-1].id+1;        }
+        else{
+            newTdodo.id=1;
+        }
+        this.tdodos.push(newTdodo);
+
+        return "Added Todos";
+
     }
-
-
-
 }
